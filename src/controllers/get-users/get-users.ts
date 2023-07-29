e5cbd3edd73e08d1bbe14c2dc96f460405f3daac
Gustavo Henrique <gustavo.henrique@gooxxy.com>
2023-07-29 16:35:24 -0300

    feat: add GetUserController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,20 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
+    try {
+      const users = await this.getUsersRepository.getUsers();
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: "Internal server error",
+      };
+    }
+  }
+}
