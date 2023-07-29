f503b176f80ac076cc5364e1c0fd80140e201835
Gustavo Henrique <gustavo.henrique@gooxxy.com>
2023-07-29 19:26:40 -0300

    feat: Delete User controller

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
-import { GetUsersController } from "./controllers/get-users/get-users";
-import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 import { MongoClient } from "./database/mongo";
-import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
-import { CreateUserController } from "./controllers/create-user/create-user";
-import { MongoUpdateUserRepository } from "./repositories/update-user/mongo-update-user";
-import { UpdateUserController } from "./controllers/update-user/update-user";
+import usersRouter from "./router/users";
 
 const main = async () => {
   config();
   const app = express();
-
   app.use(express.json());
-
+  app.use(cors());
+  app.use(bodyParser.json());
   await MongoClient.connect();
-
-  app.get("/users", async (req, res) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-    const { body, statusCode } = await getUsersController.handle();
-    res.status(statusCode).send(body);
-  });
-
-  app.post("/users", async (req, res) => {
-    const mongoCreateUserRepository = new MongoCreateUserRepository();
-    const createUserController = new CreateUserController(
-      mongoCreateUserRepository
-    );
-    const { body, statusCode } = await createUserController.handle({
-      body: req.body,
-    });
-    res.status(statusCode).send(body);
-  });
-
-  app.patch("/users/:id", async (req, res) => {
-    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
-    const updateUserController = new UpdateUserController(
-      mongoUpdateUserRepository
-    );
-
-    const { body, statusCode } = await updateUserController.handle({
-      params: req.params,
-      body: req.body,
-    });
-
-    res.status(statusCode).send(body);
-  });
+  app.use("/users", usersRouter);
 
   const port = process.env.PORT || 8000;
   app.listen(port, () => console.log(`listening on port ${port}...`));
