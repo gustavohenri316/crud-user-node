28b8d3ff9c0267b30e95f7b8f25e83ac6e7073a7
Gustavo Henrique <gustavo.henrique@gooxxy.com>
2023-07-29 17:23:37 -0300

    feat: add MongoCreateUserRepository

---
+++ src/controllers/create-user/protocols.ts
@@ -0,0 +1,11 @@
+import { User } from "../../models/user";
+
+export interface CreateUserParams {
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+}
+export interface ICreateUserRepository {
+  createUser(params: CreateUserParams): Promise<User>;
+}
